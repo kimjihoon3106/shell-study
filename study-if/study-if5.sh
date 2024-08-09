00db31b19ae383344876d7a7fca0af84f1771546
kimjihoon3106 <s24021@gsm.hs.kr>
2024-08-09 22:57:16 +0900

    study-if5

---
+++ study-if/study-if5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+number=30
+
+if [ $number -gt 10 ]; then
+	if [ $number -lt 20 ]; then
+		echo "$number 는 10보다 크고 20 보다 작습니다."
+	else
+		echo "$number 는 20보다 큽니다."
+	fi
+else
+	echo "$number 는 10 이하입니다."
+fi
